8be48a9093ef6a7acc3a415407a4d9b7cbc6f046
loongfee <vapd.radi@gmail.com>
2026-03-04 11:38:45 +0800

    updated site 20260304

--- run.bat
+++ run.bat
@@ -8,6 +8,9 @@ REM Optional proxies (comment out if not needed)
 SET "http_proxy=http://192.168.66.105:7890"
 SET "https_proxy=http://192.168.66.105:7890"
 
+git config --global http.proxy http://127.0.0.1:7890
+git config --global https.proxy http://127.0.0.1:7890
+
 REM Build a minimal PATH required by PortableJekyll, plus System32 for built-ins like chcp
 SET "PATH=%SystemRoot%\System32;%PortableJekyll%\ruby\bin;%PortableJekyll%\devkit\bin;%PortableJekyll%\git\bin;%PortableJekyll%\Python\App;%PortableJekyll%\Python\App\Scripts;%PortableJekyll%\devkit\mingw\bin;%PortableJekyll%\curl\bin"
 SET "SSL_CERT_FILE=%PortableJekyll%\curl\bin\cacert.pem"
